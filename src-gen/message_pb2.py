a1c7e980195d2bf5a08f376c399f22f19af992ed
Bibil Thaysose <greg@luzene.com>
2018-05-12 11:46:21 -0600

    adding subject to event

--- src-gen/message_pb2.py
+++ src-gen/message_pb2.py
@@ -19,7 +19,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   name='message.proto',
   package='',
   syntax='proto3',
-  serialized_pb=_b('\n\rmessage.proto\"\'\n\x05\x45vent\x12\x0c\n\x04type\x18\x01 \x01(\x03\x12\x10\n\x08\x61rgument\x18\x02 \x01(\t\"\x17\n\x07Subject\x12\x0c\n\x04\x61\x64\x64r\x18\x01 \x01(\x03\"$\n\x06Status\x12\n\n\x02ok\x18\x01 \x01(\x08\x12\x0e\n\x06status\x18\x02 \x01(\t2J\n\x0bMemoService\x12\x1b\n\x08SendMemo\x12\x06.Event\x1a\x07.Status\x12\x1e\n\tSubscribe\x12\x08.Subject\x1a\x07.Statusb\x06proto3')
+  serialized_pb=_b('\n\rmessage.proto\"\'\n\x05\x45vent\x12\x0c\n\x04subj\x18\x01 \x01(\x03\x12\x10\n\x08\x61rgument\x18\x02 \x01(\t\"\x17\n\x07Subject\x12\x0c\n\x04\x61\x64\x64r\x18\x01 \x01(\x03\"$\n\x06Status\x12\n\n\x02ok\x18\x01 \x01(\x08\x12\x0e\n\x06status\x18\x02 \x01(\t2J\n\x0bMemoService\x12\x1b\n\x08SendMemo\x12\x06.Event\x1a\x07.Status\x12\x1e\n\tSubscribe\x12\x08.Subject\x1a\x07.Statusb\x06proto3')
 )
 _sym_db.RegisterFileDescriptor(DESCRIPTOR)
 
@@ -34,7 +34,7 @@ _EVENT = _descriptor.Descriptor(
   containing_type=None,
   fields=[
     _descriptor.FieldDescriptor(
-      name='type', full_name='Event.type', index=0,
+      name='subj', full_name='Event.subj', index=0,
       number=1, type=3, cpp_type=2, label=1,
       has_default_value=False, default_value=0,
       message_type=None, enum_type=None, containing_type=None,
